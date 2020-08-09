cf227b8ae7f528fc7cd447e9201ccf5c797a2bc5
somerandomdev49 <mike.redko49@gmail.com>
2020-08-10 00:12:35 +0300

    eof fi

--- card_parser.h
+++ card_parser.h
@@ -8,7 +8,7 @@
 #define LOG(x) puts(x);
 #define LOGC(c) putc(c, stdout);
 #define LOGF(x, ...) printf(x "\n", __VA_ARGS__);
-#define EOF 0
+#define IS_EOF(x) (x == 0 && x == EOF)
 
 typedef struct Token { int type; const char *val; } Token;
 typedef struct { char *input; } Tokenizer;
@@ -19,12 +19,12 @@ enum
 
 Token next_token(Tokenizer *t)
 {
-	if(*t->input == EOF) return (Token){TT_EOF, "<eof>"};
-	while(*t->input != EOF && isspace(*t->input)) ++t->input;
-	if(*t->input != EOF && (isalpha(*t->input) || *t->input == '_'))
+	if(IS_EOF(*t->input)) return (Token){TT_EOF, "<eof>"};
+	while((!IS_EOF(*t->input)) && isspace(*t->input)) ++t->input;
+	if((!IS_EOF(*t->input)) && (isalpha(*t->input) || *t->input == '_'))
 	{
 		size_t start = t->input;
-		while(*t->input != EOF && (isalnum(*t->input) || *t->input == '_')) ++t->input;
+		while((!IS_EOF(*t->input)) && (isalnum(*t->input) || *t->input == '_')) ++t->input;
 		size_t len = t->input - start;
 		LOGF("len: %d", len);
 		LOGF(" %s ", t->input-len);
